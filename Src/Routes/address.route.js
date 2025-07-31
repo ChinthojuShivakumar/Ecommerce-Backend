21c596d17c49c20909b7d5b6e3edc0f3c10a40d4
CHINTHOJU SHIVAKUMAR <shivakumarchinthoju@gmail.com>
2025-07-31 14:34:51 +0530

    auth removed

--- Src/Routes/address.route.js
+++ Src/Routes/address.route.js
@@ -12,7 +12,7 @@ import { authentication } from "../Middleware/Auth.js";
 const addressRoute = express.Router();
 
 addressRoute.post("/address/create", authentication, createAddress);
-addressRoute.get("/address", authentication, fetchAddress);
+addressRoute.get("/address", fetchAddress);
 addressRoute.put("/address/:_id", authentication, updateAddress);
 addressRoute.delete("/address/:_id", authentication, deleteAddress);
 addressRoute.patch("/address/:_id", authentication, setDefaultAddress);
--- Src/Routes/cart.route.js
+++ Src/Routes/cart.route.js
@@ -10,7 +10,7 @@ import { authentication } from "../Middleware/Auth.js";
 const cartRoute = express.Router();
 
 cartRoute.post("/cart/create", authentication, createCartList);
-cartRoute.get("/cart/list", authentication, fetchCartList);
+cartRoute.get("/cart/list", fetchCartList);
 cartRoute.put("/cart/update", authentication, updateCart);
 cartRoute.delete("/cart/:_id", authentication, deleteCartItem);
 
