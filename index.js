5b7a588c35b0488db0007df658e0d7222ebab0f4
CHINTHOJU SHIVAKUMAR <shivakumarchinthoju@gmail.com>
2025-08-04 10:07:56 +0530

    origin

--- index.js
+++ index.js
@@ -34,25 +34,32 @@ const __dirname = path.dirname(__filename);
 //   })
 // );
 
-const allowedOrigins = [
-  process.env.FRONT_END_WEBSITE_URL,
-  process.env.FRONT_END_TEST_URL,
-  "http://localhost:5173", // Explicitly allow local React dev server
-];
+// const allowedOrigins = [
+//   process.env.FRONT_END_WEBSITE_URL,
+//   process.env.FRONT_END_TEST_URL,
+//   "http://localhost:5173", // Explicitly allow local React dev server
+// ];
+
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       if (!origin || allowedOrigins.includes(origin)) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error("Not allowed by CORS: " + origin));
+//       }
+//     },
+//     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+//     // credentials: true, // if you use cookies or auth headers
+//   })
+// );
 
 app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS: " + origin));
-      }
-    },
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-    // credentials: true, // if you use cookies or auth headers
-  })
+  cors({ origin: "*", methods: ["GET", "POST", "PUT", "PATCH", "DELETE"] })
 );
+
+// Allow preflight requests for all routes
+// app.options("/*", cors());
 
 app.use(morgan(process.env.NODE_ENV === "production" ? "combined" : "dev"));
 app.use(express.json());
